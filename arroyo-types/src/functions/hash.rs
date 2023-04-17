f6783a5d6d8816c62ceb4795815696d6abcae18d
Alex Kennedy <alex.kennedy@colorado.edu>
2023-04-17 18:37:31 -0500

    change signature for hash functions to match postgres
    md5 uses hex instead of base64
    sha is now signature fn(Vec<u8>) -> Vec<u8> because should have input and output that is DataType::Binary

--- arroyo-types/src/functions/hash.rs
+++ arroyo-types/src/functions/hash.rs
@@ -1,37 +1,33 @@
-use base64::{engine::general_purpose::STANDARD_NO_PAD as encoder, Engine};
+use hex;
 use md5::{Digest, Md5};
 
 pub fn md5(argument: String) -> String {
     let mut hasher = Md5::new();
     hasher.update(argument.as_bytes());
     let res = hasher.finalize();
-    encoder.encode(&res)
+    hex::encode(&res)
 }
 
-pub fn sha224(argument: String) -> String {
+pub fn sha224(argument: Vec<u8>) -> Vec<u8> {
     let mut hasher = sha2::Sha224::new();
-    hasher.update(argument.as_bytes());
-    let res = hasher.finalize();
-    encoder.encode(&res)
+    hasher.update(argument);
+    hasher.finalize().to_vec()
 }
 
-pub fn sha256(argument: String) -> String {
+pub fn sha256(argument: Vec<u8>) -> Vec<u8> {
     let mut hasher = sha2::Sha256::new();
-    hasher.update(argument.as_bytes());
-    let res = hasher.finalize();
-    encoder.encode(&res)
+    hasher.update(argument);
+    hasher.finalize().to_vec()
 }
 
-pub fn sha384(argument: String) -> String {
+pub fn sha384(argument: Vec<u8>) -> Vec<u8> {
     let mut hasher = sha2::Sha384::new();
-    hasher.update(argument.as_bytes());
-    let res = hasher.finalize();
-    encoder.encode(&res)
+    hasher.update(argument);
+    hasher.finalize().to_vec()
 }
 
-pub fn sha512(argument: String) -> String {
+pub fn sha512(argument: Vec<u8>) -> Vec<u8> {
     let mut hasher = sha2::Sha512::new();
-    hasher.update(argument.as_bytes());
-    let res = hasher.finalize();
-    encoder.encode(&res)
+    hasher.update(argument);
+    hasher.finalize().to_vec()
 }
