4be0b98d00a70e7d3fda48ef790bff6ba2138c6c
Jonah Eisen <jonaheisen1@gmail.com>
2023-05-18 13:47:11 +0800

    Use SWR for fetching data for job details page
    Use the SWR library to provide react hooks for fetching the data shown
    on the job details page. All the data fetching logic for that page
    (excluding the streaming request for now) has been moved to
    data_fetching.ts.
    
    Add an JobError component that's shown if the job can't be fetched,
    whether because it doesn't exist or for any other reason.
    
    Also refactor JobDetails.tsx and separate it into several components.
    
    Resolves: #72

---
+++ arroyo-console/src/components/Checkpoints.tsx
@@ -0,0 +1,149 @@
+import { CheckpointOverview, JobDetailsResp } from '../gen/api_pb';
+import React, { useState } from 'react';
+import {
+  Badge,
+  Box,
+  Flex,
+  Heading,
+  Link,
+  ListItem,
+  Stat,
+  StatGroup,
+  StatLabel,
+  StatNumber,
+  Text,
+  UnorderedList,
+} from '@chakra-ui/react';
+import * as util from '../lib/util';
+import OperatorCheckpoint from './OperatorCheckpoint';
+import { useCheckpointDetails } from '../lib/data_fetching';
+
+export interface CheckpointsProps {
+  job: JobDetailsResp;
+  checkpoints: Array<CheckpointOverview>;
+}
+
+function formatDurationHMS(micros: number): string {
+  let millis = micros / 1000;
+  let h = Math.floor(millis / 1000 / 60 / 60);
+  let m = Math.floor((millis - h * 1000 * 60 * 60) / 1000 / 60);
+  let s = Math.floor((millis - h * 1000 * 60 * 60 - m * 1000 * 60) / 1000);
+
+  return `${String(h).padStart(2, '0')}:${String(m).padStart(2, '0')}:${String(s).padStart(
+    2,
+    '0'
+  )}`;
+}
+
+const Checkpoints: React.FC<CheckpointsProps> = ({ job, checkpoints }) => {
+  const [epoch, setEpoch] = useState<number | undefined>(undefined);
+  const { checkpoint } = useCheckpointDetails(job.jobStatus?.jobId, epoch);
+
+  let details =
+    checkpoints.length > 0 ? (
+      <Text>Select checkpoint</Text>
+    ) : (
+      <Text textStyle="italic">No checkpoints</Text>
+    );
+
+  if (checkpoint != null) {
+    let start = Number(checkpoint.overview?.startTime);
+    let end = Number(
+      checkpoint.overview?.finishTime != undefined
+        ? checkpoint.overview?.finishTime
+        : new Date().getTime() * 1000
+    );
+
+    let w = 500;
+
+    function scale(x: number): number {
+      return ((x - start) / (end - start)) * w;
+    }
+
+    let ops = job.jobGraph?.nodes
+      .slice()
+      .map(n => {
+        return {
+          id: n.nodeId,
+          name: n.operator,
+          parallelism: n.parallelism,
+          checkpoint: checkpoint?.operators[n.nodeId],
+        };
+      })
+      .sort((a, b) => Number(a.id.split('_')[1]) - Number(b.id.split('_')[1]));
+
+    let checkpointBytes = Object.values(checkpoint.operators)
+      .flatMap(op => Object.values(op.tasks).map(t => (t.bytes == null ? 0 : Number(t.bytes))))
+      .reduce((a, b) => a + b, 0);
+
+    details = (
+      <Box>
+        <Heading size="md">
+          Checkpoint {epoch}
+          <Badge marginLeft={2}>{checkpoint.overview?.backend}</Badge>
+        </Heading>
+        <StatGroup width={718} border="1px solid #666" borderRadius="5px" marginTop={5} padding={3}>
+          <Stat>
+            <StatLabel>Started</StatLabel>
+            <StatNumber>
+              {new Intl.DateTimeFormat('en-us', {
+                dateStyle: undefined,
+                timeStyle: 'medium',
+              }).format(new Date(Number(checkpoint.overview?.startTime) / 1000))}
+            </StatNumber>
+          </Stat>
+          <Stat marginLeft={10}>
+            <StatLabel>Finished</StatLabel>
+            <StatNumber>
+              {checkpoint.overview?.finishTime != null
+                ? new Intl.DateTimeFormat('en-us', {
+                    dateStyle: undefined,
+                    timeStyle: 'medium',
+                  }).format(new Date(Number(checkpoint.overview?.finishTime) / 1000))
+                : '-'}
+            </StatNumber>
+          </Stat>
+          <Stat marginLeft={10}>
+            <StatLabel>Duration</StatLabel>
+            <StatNumber>{formatDurationHMS(end - start)}</StatNumber>
+          </Stat>
+          <Stat marginLeft={10}>
+            <StatLabel>Size</StatLabel>
+            <StatNumber> {util.dataFormat(checkpointBytes)} </StatNumber>
+          </Stat>
+        </StatGroup>
+
+        <Box marginTop={5}>
+          {ops?.map(op => (
+            <OperatorCheckpoint key={op.id} op={op} scale={scale} w={w} />
+          ))}
+        </Box>
+      </Box>
+    );
+  }
+
+  return (
+    <Flex>
+      <Box w="100px">
+        <UnorderedList className="checkpoint-menu">
+          {checkpoints
+            .slice()
+            .reverse()
+            .map(c => {
+              return (
+                <ListItem
+                  key={'a' + String(c.epoch)}
+                  className={c.epoch == epoch ? 'selected' : ''}
+                >
+                  <Link onClick={() => setEpoch(c.epoch)}> {c.epoch}</Link>
+                </ListItem>
+              );
+            })}
+        </UnorderedList>
+      </Box>
+      <Box>{details}</Box>
+    </Flex>
+  );
+};
+
+export default Checkpoints;
---
+++ arroyo-console/src/components/OperatorDetail.tsx
@@ -0,0 +1,101 @@
+import { JobGraph, JobMetricsResp } from '../gen/api_pb';
+import { Badge, Box, Code, HStack, Spacer } from '@chakra-ui/react';
+import { getOperatorBackpressure } from '../lib/util';
+import React from 'react';
+import { TimeSeriesGraph } from './TimeSeriesGraph';
+import Loading from './Loading';
+
+export interface OperatorDetailProps {
+  operator_id: string;
+  graph?: JobGraph;
+  metrics?: JobMetricsResp;
+}
+
+const OperatorDetail: React.FC<OperatorDetailProps> = ({ operator_id, graph, metrics }) => {
+  if (graph == undefined || metrics == undefined || metrics.metrics?.[operator_id] == undefined) {
+    return <Loading size={'lg'} />;
+  } else {
+    const node = graph.nodes.find(n => n.nodeId == operator_id);
+    const node_metrics = metrics.metrics[operator_id];
+
+    const backpressure = getOperatorBackpressure(metrics, operator_id);
+
+    let backpressureBadge;
+    if (backpressure < 0.33) {
+      backpressureBadge = <Badge colorScheme={'green'}>LOW</Badge>;
+    } else if (backpressure < 0.66) {
+      backpressureBadge = <Badge colorScheme={'yellow'}>MEDIUM</Badge>;
+    } else {
+      backpressureBadge = <Badge colorScheme={'red'}>HIGH</Badge>;
+    }
+
+    let msgRecv = 0;
+    let msgSent = 0;
+    let msgSentData;
+    let msgRecvData;
+
+    if (
+      node_metrics != null &&
+      node_metrics.subtasks != null &&
+      node_metrics.subtasks[0].messagesRecv.length > 0
+    ) {
+      msgRecv = Object.values(node_metrics.subtasks)
+        .map(n => n.messagesRecv)
+        .reduce((s, a) => s + a[a.length - 1].value, 0);
+      msgSent = Object.values(node_metrics.subtasks)
+        .map(n => n.messagesSent)
+        .reduce((s, a) => s + a[a.length - 1].value, 0);
+
+      msgRecvData = Object.entries(node_metrics.subtasks).map(kv => {
+        return kv[1].messagesRecv
+          .filter(m => m != undefined)
+          .map(m => {
+            return {
+              label: kv[0],
+              x: new Date(Number(m.time) / 1000),
+              y: m.value + m.value * Math.random() * 0.01,
+            };
+          });
+      });
+
+      msgSentData = Object.entries(node_metrics.subtasks).map(kv => {
+        return kv[1].messagesSent.map(m => {
+          return {
+            label: kv[0],
+            x: new Date(Number(m.time) / 1000),
+            y: m.value + m.value * Math.random() * 0.01,
+          };
+        });
+      });
+    }
+
+    return (
+      <Box className="operatorDetail" marginTop={10} border="1px solid #333" padding="10px">
+        <HStack fontWeight="semibold">
+          <Box>operator</Box>
+          <Spacer />
+          <Box border="1px solid #aaa" px={1.5} fontSize={12} title="parallelism for this operator">
+            {node?.parallelism}
+          </Box>
+        </HStack>
+        <Box marginTop="10px">Backpressure: {backpressureBadge}</Box>
+        <Box marginTop="10px">{node?.operator}</Box>
+        <Box marginTop="10px" fontFamily="monaco,ubuntu mono,fixed-width">
+          <Code>{Math.round(msgRecv)} eps</Code> rx
+          <Code marginLeft="20px">{Math.round(msgSent)} eps</Code> tx
+        </Box>
+        <Box className="chart" marginTop="20px" fontSize={14}>
+          Events RX
+          <TimeSeriesGraph data={msgRecvData} timeWindowMs={5 * 60 * 1000} />
+        </Box>
+
+        <Box className="chart" marginTop="20px" fontSize={14}>
+          Events TX
+          <TimeSeriesGraph data={msgSentData} timeWindowMs={5 * 60 * 1000} />
+        </Box>
+      </Box>
+    );
+  }
+};
+
+export default OperatorDetail;
