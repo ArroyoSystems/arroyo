6ecd254934317a404c268ad0052066b61d84bb36
Jonah Eisen <jonaheisen1@gmail.com>
2023-07-10 14:39:31 -0400

    Add pipeline endpoints to REST API
    Add endpoints for managing pipelines to the REST API. Creating a
    pipeline automatically creates a single job for the pipeline.
    
    Note that the REST API depends on the gRPC API so that it can use the
    'start_or_preview' function in the POST /v1/pipelines endpoint. When we
    are ready to deprecate the gRPC API we can move that logic into
    pipelines.rs.

--- arroyo-api/src/rest.rs
+++ arroyo-api/src/rest.rs
@@ -1,32 +1,106 @@
+use axum::body::Body;
 use axum::response::IntoResponse;
-use axum::{routing::get, Json, Router};
+use axum::{
+    routing::{delete, get, patch, post},
+    Json, Router,
+};
+use deadpool_postgres::Pool;
+use http::StatusCode;
+use once_cell::sync::Lazy;
+use std::env;
+use std::path::PathBuf;
+use std::str::FromStr;
+use tower::service_fn;
+use tower_http::cors;
+use tower_http::cors::CorsLayer;
+use tower_http::services::ServeDir;
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 
-#[derive(OpenApi)]
-#[openapi(
-    info(title = "Arroyo REST API", version = "1.0.0"),
-    paths(ping),
-    tags(
-        (name = "ping", description = "Ping")
-    )
-)]
-pub struct ApiDoc;
+use crate::pipelines::{
+    delete_pipeline, get_jobs, get_pipeline, get_pipelines, patch_pipeline, post_pipeline,
+};
+use crate::rest_utils::ErrorResp;
+use crate::ApiDoc;
+use crate::ApiServer;
+use arroyo_types::{telemetry_enabled, API_ENDPOINT_ENV, ASSET_DIR_ENV};
+
+#[derive(Clone)]
+pub struct AppState {
+    pub(crate) grpc_api_server: ApiServer,
+    pub(crate) pool: Pool,
+}
 
 #[utoipa::path(
     get,
     path = "/v1/ping",
     tag = "ping",
     responses(
-        (status = 200, description = "Ping endpoint",),
+        (status = 200, description = "Ping endpoint"),
     ),
 )]
 pub async fn ping() -> impl IntoResponse {
     Json("Pong")
 }
 
-pub fn create_rest_app() -> Router {
+pub async fn api_fallback() -> impl IntoResponse {
+    ErrorResp {
+        status_code: StatusCode::NOT_FOUND,
+        message: "Route not found.".to_string(),
+    }
+}
+
+pub fn create_rest_app(server: ApiServer, pool: Pool) -> Router {
+    let asset_dir = env::var(ASSET_DIR_ENV).unwrap_or_else(|_| "arroyo-console/dist".to_string());
+
+    static INDEX_HTML: Lazy<String> = Lazy::new(|| {
+        let asset_dir =
+            env::var(ASSET_DIR_ENV).unwrap_or_else(|_| "arroyo-console/dist".to_string());
+
+        let endpoint = env::var(API_ENDPOINT_ENV).unwrap_or_else(|_| String::new());
+
+        std::fs::read_to_string(PathBuf::from_str(&asset_dir).unwrap()
+            .join("index.html"))
+            .expect("Could not find index.html in asset dir (you may need to build the console sources)")
+            .replace("{{API_ENDPOINT}}", &endpoint)
+            .replace("{{CLUSTER_ID}}", &arroyo_server_common::get_cluster_id())
+            .replace("{{DISABLE_TELEMETRY}}", if telemetry_enabled() { "false" } else { "true" })
+    });
+
+    let fallback = service_fn(|_: http::Request<_>| async move {
+        let body = Body::from(INDEX_HTML.as_str());
+        let res = http::Response::new(body);
+        Ok::<_, _>(res)
+    });
+
+    let serve_dir = ServeDir::new(&asset_dir).not_found_service(fallback);
+
+    // TODO: enable in development only!!!
+    let cors = CorsLayer::new()
+        .allow_headers(cors::Any)
+        .allow_origin(cors::Any);
+
+    let api_routes = Router::new()
+        .route("/ping", get(ping))
+        .route("/pipelines", post(post_pipeline))
+        .route("/pipelines", get(get_pipelines))
+        .route("/pipelines/:id", patch(patch_pipeline))
+        .route("/pipelines/:id", get(get_pipeline))
+        .route("/pipelines/:id", delete(delete_pipeline))
+        .route("/pipelines/:id/jobs", get(get_jobs))
+        .fallback(api_fallback);
+
     Router::new()
-        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
-        .route("/v1/ping", get(ping))
+        .merge(
+            SwaggerUi::new("/api/v1/swagger-ui")
+                .url("/api/v1/api-docs/openapi.json", ApiDoc::openapi()),
+        )
+        .nest("/api/v1", api_routes)
+        .route_service("/", fallback)
+        .fallback_service(serve_dir)
+        .with_state(AppState {
+            grpc_api_server: server,
+            pool,
+        })
+        .layer(cors)
 }
