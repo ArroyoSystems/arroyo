d7626f8d248720fb21bf2a3fa56069da7d7488ed
Jonah Eisen <jonaheisen1@gmail.com>
2023-07-10 14:39:31 -0400

    Add pipeline endpoints to REST API
    Add endpoints for managing pipelines to the REST API. Creating a
    pipeline automatically creates a single job for the pipeline.
    
    Note that the REST API depends on the gRPC API so that it can use the
    'start_or_preview' function in the POST /v1/pipelines endpoint. When we
    are ready to deprecate the gRPC API we can move that logic into
    pipelines.rs.

---
+++ arroyo-api/src/rest_types.rs
@@ -0,0 +1,81 @@
+use crate::types::public::StopMode;
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+
+#[derive(Serialize, Deserialize, Clone, Debug, ToSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct PipelinePost {
+    pub name: String,
+    pub query: String,
+    pub udfs: Vec<Udf>,
+    pub preview: Option<bool>,
+    pub parallelism: u64,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, ToSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct Pipeline {
+    pub id: String,
+    pub name: String,
+    pub query: String,
+    pub udfs: Vec<Udf>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, ToSchema)]
+#[serde(rename_all = "camelCase")]
+pub enum StopType {
+    None,
+    Checkpoint,
+    Graceful,
+    Immediate,
+    Force,
+}
+
+impl From<StopMode> for StopType {
+    fn from(value: StopMode) -> Self {
+        match value {
+            StopMode::none => StopType::None,
+            StopMode::checkpoint => StopType::Checkpoint,
+            StopMode::graceful => StopType::Graceful,
+            StopMode::immediate => StopType::Immediate,
+            StopMode::force => StopType::Force,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, ToSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct Job {
+    pub id: String,
+    pub running_desired: bool,
+    pub state: String,
+    pub run_id: u64,
+    pub start_time: Option<u64>,
+    pub finish_time: Option<u64>,
+    pub tasks: Option<u64>,
+    pub failure_message: Option<String>,
+    pub checkpoint_interval_micros: u64,
+    pub stop: StopType,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, ToSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct Udf {
+    pub definition: String,
+}
+
+// Collections need to be created with this macro rather than a generic type
+// because utoipa::ToSchema (and the OpenAPI spec) don't support generics natively
+macro_rules! collection_type {
+    ($struct_name:ident, $item_type:ty) => {
+        #[derive(Serialize, Deserialize, Clone, Debug, ToSchema)]
+        #[serde(rename_all = "camelCase")]
+        pub struct $struct_name {
+            pub data: Vec<$item_type>,
+            pub has_more: bool,
+        }
+    };
+}
+
+collection_type!(JobCollection, Job);
+collection_type!(PipelineCollection, Pipeline);
