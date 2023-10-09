c386796d110359f53d803ebd5b9c2f9bff2a8d5e
Jonah Eisen <jonaheisen1@gmail.com>
2023-10-09 11:32:49 -0700

    Add session window smoke tests

--- arroyo-sql-testing/src/smoke_tests.rs
+++ arroyo-sql-testing/src/smoke_tests.rs
@@ -758,3 +758,34 @@ INSERT INTO union_output
 SELECT counter FROM (SELECT counter  FROM impulse_source)
 UNION ALL (SELECT 2 * counter FROM impulse_source)"
 }
+
+correctness_run_codegen! {"session_window", 10,
+"CREATE TABLE impulse_source (
+  timestamp TIMESTAMP,
+  counter bigint unsigned not null,
+  subtask_index bigint unsigned not null
+) WITH (
+  connector = 'single_file',
+  path = '$input_dir/impulse.json',
+  format = 'json',
+  type = 'source',
+  event_time_field = 'timestamp'
+);
+
+CREATE TABLE session_window_output (
+  start timestamp,
+  end timestamp,
+  user_id bigint,
+  rows bigint,
+) WITH (
+  connector = 'single_file',
+  path = '$output_path',
+  format = 'json',
+  type = 'sink'
+);
+
+INSERT INTO session_window_output
+SELECT window.start, window.end, user_id, rows FROM (
+    SELECT SESSION(interval '20 seconds') as window, Case when counter%10=0 then 0 else counter END as user_id, count(*) as rows
+    FROM impulse_source GROUP BY window, user_id);
+"}
