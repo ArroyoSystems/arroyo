783ef47e8ac19ee4e5b03d4e518296c45fb2950e
Jackson Newhouse <jackson@arroyo.systems>
2024-02-02 12:11:53 -0800

    refactor filesystem source. (#519)

--- arroyo-connectors/src/filesystem/source.rs
+++ arroyo-connectors/src/filesystem/source.rs
@@ -1,11 +1,16 @@
+use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
 use std::future::ready;
+use std::hash::{Hash, Hasher};
 use std::time::SystemTime;
 
 use anyhow::Result;
 use arrow::array::RecordBatch;
 
+use arrow::datatypes::SchemaRef;
 use arroyo_state::global_table_config;
+use arroyo_state::tables::global_keyed_map::GlobalKeyedView;
+use async_compression::tokio::bufread::{GzipDecoder, ZstdDecoder};
 use async_trait::async_trait;
 use bincode::{Decode, Encode};
 use datafusion::common::ScalarValue;
@@ -15,14 +20,16 @@ use parquet::arrow::ParquetRecordBatchStreamBuilder;
 
 use arroyo_operator::context::ArrowContext;
 use regex::Regex;
+use tokio::io::{AsyncBufReadExt, AsyncRead, BufReader};
 use tokio::select;
+use tokio_stream::wrappers::LinesStream;
 use tokio_stream::Stream;
-use tracing::{info, warn};
+use tracing::info;
 
 use crate::filesystem::{CompressionFormat, TableType};
 use arroyo_operator::operator::SourceOperator;
 use arroyo_operator::SourceFinishType;
-use arroyo_rpc::formats::{BadData, Format};
+use arroyo_rpc::formats::{BadData, Format, Framing};
 use arroyo_rpc::grpc::TableConfig;
 use arroyo_rpc::{grpc::StopMode, ControlMessage};
 use arroyo_storage::StorageProvider;
@@ -32,6 +39,7 @@ use arroyo_types::{to_nanos, UserError};
 pub struct FileSystemSourceFunc {
     pub table: TableType,
     pub format: Format,
+    pub framing: Option<Framing>,
     pub bad_data: Option<BadData>,
     pub file_states: HashMap<String, FileReadState>,
 }
@@ -78,10 +86,6 @@ impl FileSystemSourceFunc {
     }
 
     async fn run_int(&mut self, ctx: &mut ArrowContext) -> Result<SourceFinishType, UserError> {
-        if ctx.task_info.task_index != 0 {
-            return Ok(SourceFinishType::Final);
-        }
-
         let (storage_provider, regex_pattern) = match &self.table {
             TableType::Source {
                 path,
@@ -114,6 +118,13 @@ impl FileSystemSourceFunc {
                 ))
             }
         };
+        ctx.initialize_deserializer(
+            self.format.clone(),
+            self.framing.clone(),
+            self.bad_data.clone(),
+        );
+        let parallelism = ctx.task_info.parallelism;
+        let task_index = ctx.task_info.task_index;
 
         // TODO: sort by creation time
         let mut file_paths = storage_provider
@@ -124,6 +135,14 @@ impl FileSystemSourceFunc {
                 let Ok(path) = path else {
                     return ready(true);
                 };
+                // hash the path and modulo by the number of tasks
+                let mut hasher = DefaultHasher::new();
+                path.hash(&mut hasher);
+                if (hasher.finish() as usize) % parallelism != task_index {
+                    info!("filtering out path {}", path.to_string());
+                    return ready(false);
+                }
+
                 if let Some(matcher) = &regex_pattern {
                     ready(matcher.is_match(&path.to_string()))
                 } else {
@@ -131,10 +150,7 @@ impl FileSystemSourceFunc {
                 }
             });
 
-        let state: &mut arroyo_state::tables::global_keyed_map::GlobalKeyedView<
-            String,
-            (String, FileReadState),
-        > = ctx
+        let state: &mut GlobalKeyedView<String, (String, FileReadState)> = ctx
             .table_manager
             .get_global_keyed_state("a")
             .await
@@ -160,15 +176,48 @@ impl FileSystemSourceFunc {
         Ok(SourceFinishType::Final)
     }
 
+    async fn get_newline_separated_stream(
+        &mut self,
+        storage_provider: &StorageProvider,
+        path: String,
+    ) -> Result<Box<dyn Stream<Item = Result<String, UserError>> + Unpin + Send>, UserError> {
+        match &self.format {
+            Format::Json(_) => {
+                let stream_reader = storage_provider.get_as_stream(path).await.unwrap();
+
+                let compression_reader: Box<dyn AsyncRead + Unpin + Send> =
+                    match self.get_compression_format() {
+                        CompressionFormat::Zstd => {
+                            Box::new(ZstdDecoder::new(BufReader::new(stream_reader)))
+                        }
+                        CompressionFormat::Gzip => {
+                            Box::new(GzipDecoder::new(BufReader::new(stream_reader)))
+                        }
+                        CompressionFormat::None => Box::new(BufReader::new(stream_reader)),
+                    };
+                // use line iterators
+                let lines = LinesStream::new(BufReader::new(compression_reader).lines());
+                Ok(Box::new(lines.map(|string_result| {
+                    string_result.map_err(|err| {
+                        UserError::new("could not read line from stream", err.to_string())
+                    })
+                })))
+            }
+            other => Err(UserError::new(
+                "bad format",
+                format!("newline separated stream not supported for {:?}", other),
+            )),
+        }
+    }
+
     async fn get_record_batch_stream(
         &mut self,
         storage_provider: &StorageProvider,
         path: String,
+        out_schema: SchemaRef,
     ) -> Result<Box<dyn Stream<Item = Result<RecordBatch, UserError>> + Unpin + Send>, UserError>
     {
         match &self.format {
-            Format::Json(_) => Err(UserError::new("unsupported format:", "json")),
-            Format::Avro(_) => Err(UserError::new("unsupported format:", "avro")),
             Format::Parquet(_) => {
                 let object_meta = storage_provider
                     .get_backing_store()
@@ -194,8 +243,27 @@ impl FileSystemSourceFunc {
                         err.to_string(),
                     )
                 })?;
-                let result = Box::new(stream.map(|res| match res {
-                    Ok(record_batch) => Ok(record_batch),
+                let result = Box::new(stream.map(move |res| match res {
+                    Ok(record_batch) => {
+                        // add timestamp
+                        let mut columns = record_batch.columns().to_vec();
+                            let current_time = to_nanos(SystemTime::now());
+                            let current_time_scalar =
+                                ScalarValue::TimestampNanosecond(Some(current_time as i64), None);
+
+                            let time_column = current_time_scalar
+                                .to_array_of_size(record_batch.num_rows())
+                                .unwrap();
+
+                            columns.push(time_column);
+
+                            let out_batch = RecordBatch::try_new(
+                                out_schema.clone(),
+                                columns
+                            ).map_err(|e| UserError::new("data does not match schema",
+                                format!("The parquet file has a schema that does not match the table schema: {:?}", e)))?;
+                                Ok(out_batch)
+                    },
                     Err(err) => Err(UserError::new(
                         "could not read record batch from stream",
                         err.to_string(),
@@ -204,7 +272,7 @@ impl FileSystemSourceFunc {
                     as Box<dyn Stream<Item = Result<RecordBatch, UserError>> + Send + Unpin>;
                 Ok(result)
             }
-            Format::RawString(_) => todo!(),
+            _ => unreachable!("code path only for Parquet"),
         }
     }
 
@@ -218,7 +286,7 @@ impl FileSystemSourceFunc {
             .file_states
             .entry(obj_key.to_string())
             .or_insert(FileReadState::RecordsRead(0));
-        let mut records_read = match read_state {
+        let records_read = match read_state {
             FileReadState::RecordsRead(records_read) => *records_read,
             FileReadState::Finished => {
                 return Err(UserError::new(
@@ -228,54 +296,89 @@ impl FileSystemSourceFunc {
             }
         };
 
-        let mut reader = self
-            .get_record_batch_stream(storage_provider, obj_key.to_string())
-            .await?;
-
-        if records_read > 0 {
-            warn!("skipping {} items", records_read);
-            for _ in 0..records_read {
-                let _ = reader.next().await.ok_or_else(|| {
-                    UserError::new(
-                        "could not skip item",
-                        format!(
-                            "based on checkpoint expected {} to have at least {} lines",
-                            obj_key, records_read
-                        ),
-                    )
-                })?;
+        match self.format {
+            Format::Json(_) => {
+                let line_reader = self
+                    .get_newline_separated_stream(storage_provider, obj_key.to_string())
+                    .await?
+                    .skip(records_read);
+                self.read_line_file(ctx, line_reader, obj_key, records_read)
+                    .await
             }
+            Format::Avro(_) => todo!(),
+            Format::Parquet(_) => {
+                let record_batch_stream = self
+                    .get_record_batch_stream(
+                        storage_provider,
+                        obj_key.to_string(),
+                        ctx.out_schema.as_ref().unwrap().schema.clone(),
+                    )
+                    .await?
+                    .skip(records_read);
+
+                self.read_parquet_file(ctx, record_batch_stream, obj_key, records_read)
+                    .await
+            }
+            Format::RawString(_) => todo!(),
         }
+    }
 
-        let out_schema = ctx.out_schema.as_ref().unwrap().clone();
-
+    async fn read_parquet_file(
+        &mut self,
+        ctx: &mut ArrowContext,
+        mut record_batch_stream: impl Stream<Item = Result<RecordBatch, UserError>> + Unpin + Send,
+        obj_key: &String,
+        mut records_read: usize,
+    ) -> Result<Option<SourceFinishType>, UserError> {
         loop {
             select! {
-                item = reader.next() => {
+                item = record_batch_stream.next() => {
                     match item.transpose()? {
                         Some(batch) => {
-                            let mut columns = batch.columns().to_vec();
-                            let current_time = to_nanos(SystemTime::now());
-                            let current_time_scalar =
-                                ScalarValue::TimestampNanosecond(Some(current_time as i64), None);
-
-                            let time_column = current_time_scalar
-                                .to_array_of_size(batch.num_rows())
-                                .unwrap();
-
-                            columns.push(time_column);
-
-                            let out_batch = RecordBatch::try_new(
-                                out_schema.schema.clone(),
-                                columns
-                            ).map_err(|e| UserError::new("data does not match schema",
-                                format!("The parquet file has a schema that does not match the table schema: {:?}", e)))?;
-
-                            ctx.collect(out_batch).await;
+                            ctx.collect(batch).await;
                             records_read += 1;
                         }
                         None => {
                             info!("finished reading file {}", obj_key);
+                            self.file_states.insert(obj_key.to_string(), FileReadState::Finished);
+                            return Ok(None);
+                        }
+                    }
+                },
+                msg_res = ctx.control_rx.recv() => {
+                    if let Some(control_message) = msg_res {
+                        self.file_states.insert(obj_key.to_string(), FileReadState::RecordsRead(records_read));
+                        match self.process_control_message(ctx, control_message).await {
+                            Some(finish_type) => return Ok(Some(finish_type)),
+                            None => ()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    async fn read_line_file(
+        &mut self,
+        ctx: &mut ArrowContext,
+        mut line_reader: impl Stream<Item = Result<String, UserError>> + Unpin + Send,
+        obj_key: &String,
+        mut records_read: usize,
+    ) -> Result<Option<SourceFinishType>, UserError> {
+        loop {
+            select! {
+                line = line_reader.next() => {
+                    match line.transpose()? {
+                        Some(line) => {
+                            ctx.deserialize_slice(line.as_bytes(), SystemTime::now()).await?;
+                            records_read += 1;
+                            if ctx.should_flush() {
+                                ctx.flush_buffer().await?;
+                            }
+                        }
+                        None => {
+                            info!("finished reading file {}", obj_key);
+                            ctx.flush_buffer().await?;
                             self.file_states.insert(obj_key.to_string(), FileReadState::Finished);
                             return Ok(None);
                         }
