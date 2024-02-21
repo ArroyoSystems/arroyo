e2324df7efaa29f976372fb8f2b5d5e7c3f71ece
Micah Wylde <micah@arroyo.systems>
2024-02-21 15:41:53 -0800

    Add raw string serialization

--- crates/arroyo-formats/src/serialize/mod.rs
+++ crates/arroyo-formats/src/serialize/mod.rs
@@ -1,7 +1,9 @@
 use crate::{avro, json};
+use arrow_array::cast::AsArray;
 use arrow_array::RecordBatch;
 use arrow_json::writer::record_batches_to_json_rows;
-use arroyo_rpc::formats::{Format, JsonFormat};
+use arrow_schema::DataType;
+use arroyo_rpc::formats::{Format, JsonFormat, RawStringFormat};
 use arroyo_rpc::TIMESTAMP_FIELD;
 use serde_json::Value;
 
@@ -38,7 +40,7 @@ impl ArrowSerializer {
             Format::Json(json) => self.serialize_json(json, batch),
             Format::Avro(_) => todo!("avro"),
             Format::Parquet(_) => todo!("parquet"),
-            Format::RawString(_) => todo!("raw string"),
+            Format::RawString(RawStringFormat {}) => self.serialize_raw_string(batch),
         }
     }
 
@@ -82,5 +84,131 @@ impl ArrowSerializer {
 
             buf
         }))
+    }
+    fn serialize_raw_string(
+        &self,
+        batch: &RecordBatch,
+    ) -> Box<dyn Iterator<Item = Vec<u8>> + Send> {
+        let value_idx = batch.schema().index_of("value").unwrap_or_else(|_| {
+            panic!(
+                "invalid schema for raw_string serializer: {}; a VALUE column is required",
+                batch.schema()
+            )
+        });
+
+        if *batch.schema().field(value_idx).data_type() != DataType::Utf8 {
+            panic!("invalid schema for raw_string serializer: {}; a must have a column VALUE of type TEXT", batch.schema());
+        }
+
+        let values: Vec<Vec<u8>> = batch
+            .column(value_idx)
+            .as_string::<i32>()
+            .iter()
+            .map(|v| v.map(|v| v.as_bytes().to_vec()).unwrap_or_default())
+            .collect();
+
+        Box::new(values.into_iter())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::serialize::ArrowSerializer;
+    use arrow_schema::{Schema, TimeUnit};
+    use arroyo_rpc::formats::{Format, RawStringFormat};
+    use arroyo_types::to_nanos;
+    use std::sync::Arc;
+    use std::time::{Duration, SystemTime};
+
+    #[test]
+    fn test_raw_string() {
+        let mut serializer = ArrowSerializer::new(Format::RawString(RawStringFormat {}));
+
+        let data: Vec<_> = vec!["a", "b", "blah", "whatever"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
+        let ts: Vec<_> = data
+            .iter()
+            .enumerate()
+            .map(|(i, _)| to_nanos(SystemTime::now() + Duration::from_secs(i as u64)) as i64)
+            .collect();
+
+        let schema = Arc::new(Schema::new(vec![
+            arrow_schema::Field::new("value", arrow_schema::DataType::Utf8, false),
+            arrow_schema::Field::new(
+                "_timestamp",
+                arrow_schema::DataType::Timestamp(TimeUnit::Nanosecond, None),
+                false,
+            ),
+        ]));
+
+        let batch = arrow_array::RecordBatch::try_new(
+            schema,
+            vec![
+                Arc::new(arrow_array::StringArray::from(data)),
+                Arc::new(arrow_array::TimestampNanosecondArray::from(ts)),
+            ],
+        )
+        .unwrap();
+
+        let mut iter = serializer.serialize(&batch);
+        assert_eq!(iter.next().unwrap(), b"a");
+        assert_eq!(iter.next().unwrap(), b"b");
+        assert_eq!(iter.next().unwrap(), b"blah");
+        assert_eq!(iter.next().unwrap(), b"whatever");
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn test_json() {
+        let mut serializer = ArrowSerializer::new(Format::Json(arroyo_rpc::formats::JsonFormat {
+            confluent_schema_registry: false,
+            schema_id: None,
+            include_schema: false,
+            debezium: false,
+            unstructured: false,
+            timestamp_format: Default::default(),
+        }));
+
+        let text: Vec<_> = vec!["a", "b", "blah", "whatever"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
+
+        let numbers = vec![1, 2, 3, 4];
+
+        let ts: Vec<_> = text
+            .iter()
+            .enumerate()
+            .map(|(i, _)| to_nanos(SystemTime::now() + Duration::from_secs(i as u64)) as i64)
+            .collect();
+
+        let schema = Arc::new(Schema::new(vec![
+            arrow_schema::Field::new("value", arrow_schema::DataType::Utf8, false),
+            arrow_schema::Field::new("number", arrow_schema::DataType::Int32, false),
+            arrow_schema::Field::new(
+                "_timestamp",
+                arrow_schema::DataType::Timestamp(TimeUnit::Nanosecond, None),
+                false,
+            ),
+        ]));
+
+        let batch = arrow_array::RecordBatch::try_new(
+            schema,
+            vec![
+                Arc::new(arrow_array::StringArray::from(text)),
+                Arc::new(arrow_array::Int32Array::from(numbers)),
+                Arc::new(arrow_array::TimestampNanosecondArray::from(ts)),
+            ],
+        )
+        .unwrap();
+
+        let mut iter = serializer.serialize(&batch);
+        assert_eq!(iter.next().unwrap(), br#"{"value":"a","number":1}"#);
+        assert_eq!(iter.next().unwrap(), br#"{"value":"b","number":2}"#);
+        assert_eq!(iter.next().unwrap(), br#"{"value":"blah","number":3}"#);
+        assert_eq!(iter.next().unwrap(), br#"{"value":"whatever","number":4}"#);
+        assert_eq!(iter.next(), None);
     }
 }
