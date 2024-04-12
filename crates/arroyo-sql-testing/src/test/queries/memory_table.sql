e6b7dd057da9c8fbcec21b935216fdadf5183124
Jackson Newhouse <jackson@arroyo.systems>
2024-04-12 23:35:03 +0000

    Support memory tables in SQL.

---
+++ crates/arroyo-sql-testing/src/test/queries/memory_table.sql
@@ -0,0 +1,29 @@
+CREATE TABLE cars (
+      timestamp TIMESTAMP,
+      driver_id BIGINT,
+      event_type TEXT,
+      location TEXT
+    ) WITH (
+      connector = 'single_file',
+      path = '$input_dir/cars.json',
+      format = 'json',
+      type = 'source'
+    );
+
+    CREATE TABLE memory (
+      event_type TEXT,
+      location TEXT,
+      driver_id BIGINT,
+    );
+
+    CREATE TABLE cars_output (
+      driver_id BIGINT,
+      event_type TEXT
+    ) WITH (
+      connector = 'single_file',
+      path = '$output_path',
+      format = 'json',
+      type = 'sink'
+    );
+    INSERT INTO memory SELECT event_type, location, driver_id as other_driver_id FROM cars;
+    INSERT INTO cars_output SELECT driver_id as other_driver_id, event_type FROM memory;
