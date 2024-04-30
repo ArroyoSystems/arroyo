ac389cb9f1cc548254c404c52fdf4f60e5b05282
Jackson Newhouse <jackson@arroyo.systems>
2024-04-30 18:20:48 +0000

    Support writing non-updating queries to debezium sinks.

---
+++ crates/arroyo-sql-testing/src/test/queries/debezium_coercion.sql
@@ -0,0 +1,24 @@
+CREATE TABLE impulse_source
+(
+    timestamp     TIMESTAMP,
+    counter       bigint unsigned not null,
+    subtask_index bigint unsigned not null
+) WITH (
+      connector = 'single_file',
+      path = '$input_dir/impulse.json',
+      format = 'json',
+      type = 'source'
+      );
+CREATE TABLE output
+(
+    counter bigint
+) WITH (
+      connector = 'single_file',
+      path = '$output_path',
+      format = 'debezium_json',
+      type = 'sink'
+);
+
+INSERT INTO output
+SELECT counter
+FROM impulse_source;
