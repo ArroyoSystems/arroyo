fadf5c914d19d1a6851175bfa854840a991e8931
Micah Wylde <micah@arroyo.systems>
2024-05-08 15:32:40 -0700

    Bake in optimizer rules

--- crates/arroyo-df/src/tables.rs
+++ crates/arroyo-df/src/tables.rs
@@ -30,6 +30,27 @@ use datafusion::{
         sqlparser::ast::{ColumnDef, ColumnOption, Statement, Value},
     },
 };
+use datafusion::optimizer::common_subexpr_eliminate::CommonSubexprEliminate;
+use datafusion::optimizer::decorrelate_predicate_subquery::DecorrelatePredicateSubquery;
+use datafusion::optimizer::eliminate_cross_join::EliminateCrossJoin;
+use datafusion::optimizer::eliminate_duplicated_expr::EliminateDuplicatedExpr;
+use datafusion::optimizer::eliminate_filter::EliminateFilter;
+use datafusion::optimizer::eliminate_join::EliminateJoin;
+use datafusion::optimizer::eliminate_limit::EliminateLimit;
+use datafusion::optimizer::eliminate_nested_union::EliminateNestedUnion;
+use datafusion::optimizer::eliminate_one_union::EliminateOneUnion;
+use datafusion::optimizer::eliminate_outer_join::EliminateOuterJoin;
+use datafusion::optimizer::extract_equijoin_predicate::ExtractEquijoinPredicate;
+use datafusion::optimizer::filter_null_join_keys::FilterNullJoinKeys;
+use datafusion::optimizer::OptimizerRule;
+use datafusion::optimizer::propagate_empty_relation::PropagateEmptyRelation;
+use datafusion::optimizer::push_down_filter::PushDownFilter;
+use datafusion::optimizer::push_down_limit::PushDownLimit;
+use datafusion::optimizer::replace_distinct_aggregate::ReplaceDistinctWithAggregate;
+use datafusion::optimizer::rewrite_disjunctive_predicate::RewriteDisjunctivePredicate;
+use datafusion::optimizer::scalar_subquery_to_join::ScalarSubqueryToJoin;
+use datafusion::optimizer::simplify_expressions::SimplifyExpressions;
+use datafusion::optimizer::unwrap_cast_in_comparison::UnwrapCastInComparison;
 
 use crate::extension::remote_table::RemoteTableExtension;
 use crate::types::convert_data_type;
@@ -99,13 +120,45 @@ fn produce_optimized_plan(
     let analyzed_plan =
         analyzer.execute_and_check(&plan, &ConfigOptions::default(), |_plan, _rule| {})?;
 
-    let mut optimizer = Optimizer::new();
-    optimizer.rules.retain(|rule|
-        // This rule can drop event time calculation fields if they aren't used elsewhere.
-        rule.name() != "optimize_projections"
-            // This rule creates nested aggregates off of count(distinct value), which we don't support.
-            && rule.name() != "single_distinct_aggregation_to_group_by");
+    let rules: Vec<Arc<dyn OptimizerRule + Send + Sync>> = vec![
+        Arc::new(EliminateNestedUnion::new()),
+        Arc::new(SimplifyExpressions::new()),
+        Arc::new(UnwrapCastInComparison::new()),
+        Arc::new(ReplaceDistinctWithAggregate::new()),
+        Arc::new(EliminateJoin::new()),
+        Arc::new(DecorrelatePredicateSubquery::new()),
+        Arc::new(ScalarSubqueryToJoin::new()),
+        Arc::new(ExtractEquijoinPredicate::new()),
+        Arc::new(SimplifyExpressions::new()),
+        Arc::new(RewriteDisjunctivePredicate::new()),
+        Arc::new(EliminateDuplicatedExpr::new()),
+        Arc::new(EliminateFilter::new()),
+        Arc::new(EliminateCrossJoin::new()),
+        Arc::new(CommonSubexprEliminate::new()),
+        Arc::new(EliminateLimit::new()),
+        Arc::new(PropagateEmptyRelation::new()),
+        // Must be after PropagateEmptyRelation
+        Arc::new(EliminateOneUnion::new()),
+        Arc::new(FilterNullJoinKeys::default()),
+        Arc::new(EliminateOuterJoin::new()),
+        // Filters can't be pushed down past Limits, we should do PushDownFilter after PushDownLimit
+        Arc::new(PushDownLimit::new()),
+        Arc::new(PushDownFilter::new()),
 
+        // This rule creates nested aggregates off of count(distinct value), which we don't support.
+        //Arc::new(SingleDistinctToGroupBy::new()),
+
+        // The previous optimizations added expressions and projections,
+        // that might benefit from the following rules
+        Arc::new(SimplifyExpressions::new()),
+        Arc::new(UnwrapCastInComparison::new()),
+        Arc::new(CommonSubexprEliminate::new()),
+
+        // This rule can drop event time calculation fields if they aren't used elsewhere.
+        //Arc::new(OptimizeProjections::new()),
+    ];
+
+    let optimizer = Optimizer::with_rules(rules);
     let plan = optimizer.optimize(
         &analyzed_plan,
         &OptimizerContext::default(),
