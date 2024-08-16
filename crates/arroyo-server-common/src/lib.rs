7e591cec3269dcdfb807775e667cdef9b94296af
Micah Wylde <micah@arroyo.systems>
2024-08-15 17:28:26 -0700

    Only use jemalloc on arm64 and x86_64

--- crates/arroyo-server-common/src/lib.rs
+++ crates/arroyo-server-common/src/lib.rs
@@ -7,6 +7,7 @@ use arroyo_types::POSTHOG_KEY;
 use axum::body::Bytes;
 use axum::extract::State;
 use axum::http::StatusCode;
+use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
 use hyper::Body;
@@ -22,7 +23,6 @@ use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::sync::Arc;
 use std::task::{Context, Poll};
-use axum::response::IntoResponse;
 use tonic::body::BoxBody;
 use tonic::transport::Server;
 use tower::layer::util::Stack;
@@ -245,7 +245,6 @@ async fn details<'a>(State(state): State<Arc<AdminState>>) -> String {
     .unwrap()
 }
 
-
 pub async fn handle_get_heap() -> Result<impl IntoResponse, (StatusCode, String)> {
     let mut prof_ctl = jemalloc_pprof::PROF_CTL.as_ref().unwrap().lock().await;
     require_profiling_activated(&prof_ctl)?;
@@ -256,11 +255,16 @@ pub async fn handle_get_heap() -> Result<impl IntoResponse, (StatusCode, String)
 }
 
 /// Checks whether jemalloc profiling is activated an returns an error response if not.
-fn require_profiling_activated(prof_ctl: &jemalloc_pprof::JemallocProfCtl) -> Result<(), (StatusCode, String)> {
+fn require_profiling_activated(
+    prof_ctl: &jemalloc_pprof::JemallocProfCtl,
+) -> Result<(), (StatusCode, String)> {
     if prof_ctl.activated() {
         Ok(())
     } else {
-        Err((axum::http::StatusCode::FORBIDDEN, "heap profiling not activated".into()))
+        Err((
+            axum::http::StatusCode::FORBIDDEN,
+            "heap profiling not activated".into(),
+        ))
     }
 }
 
