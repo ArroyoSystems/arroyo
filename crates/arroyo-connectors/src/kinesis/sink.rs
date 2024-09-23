20af158fedcc69438533eccad39939630a22db84
Micah Wylde <micah@arroyo.systems>
2024-09-23 10:59:43 -0700

    Upgrade kinesis dependency to avoid multiple AWS client versions (#747)

--- crates/arroyo-connectors/src/kinesis/sink.rs
+++ crates/arroyo-connectors/src/kinesis/sink.rs
@@ -1,22 +1,24 @@
-use std::time::{Duration, SystemTime};
+use std::collections::HashSet;
+use std::sync::Arc;
+use std::time::{Duration, Instant};
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use arrow::array::RecordBatch;
 use arroyo_formats::ser::ArrowSerializer;
 use arroyo_operator::context::ArrowContext;
 use arroyo_operator::operator::ArrowOperator;
+use arroyo_rpc::retry;
 use arroyo_types::CheckpointBarrier;
 use async_trait::async_trait;
-use aws_config::from_env;
-use aws_sdk_kinesis::{
-    client::fluent_builders::PutRecords, model::PutRecordsRequestEntry, types::Blob,
-    Client as KinesisClient, Region,
-};
+use aws_config::{from_env, Region};
+use aws_sdk_kinesis::primitives::Blob;
+use aws_sdk_kinesis::types::PutRecordsRequestEntry;
+use aws_sdk_kinesis::Client as KinesisClient;
 use tracing::warn;
 use uuid::Uuid;
 
 pub struct KinesisSinkFunc {
-    pub client: Option<KinesisClient>,
+    pub client: Option<Arc<KinesisClient>>,
     pub aws_region: Option<String>,
     pub in_progress_batch: Option<BatchRecordPreparer>,
     pub flush_config: FlushConfig,
@@ -35,103 +37,72 @@ impl ArrowOperator for KinesisSinkFunc {
         if let Some(region) = &self.aws_region {
             loader = loader.region(Region::new(region.clone()));
         }
-        self.client = Some(KinesisClient::new(&loader.load().await));
+
+        let client = Arc::new(KinesisClient::new(&loader.load().await));
+        self.client = Some(client.clone());
+
+        self.in_progress_batch = Some(BatchRecordPreparer::new(client, self.name.clone()));
     }
 
-    async fn process_batch(&mut self, batch: RecordBatch, _ctx: &mut ArrowContext) {
-        let mut batch_preparer = match self.in_progress_batch.take() {
-            None => BatchRecordPreparer::new(
-                self.client
-                    .as_ref()
-                    .unwrap()
-                    .put_records()
-                    .stream_name(self.name.clone()),
-            ),
-            Some(batch_preparer) => batch_preparer,
-        };
-
+    async fn process_batch(&mut self, batch: RecordBatch, ctx: &mut ArrowContext) {
         for v in self.serializer.serialize(&batch) {
-            batch_preparer.add_record(Uuid::new_v4().to_string(), v);
-        }
-
-        if self.flush_config.should_flush(&batch_preparer) {
-            self.flush_with_retries(batch_preparer)
+            self.in_progress_batch
+                .as_mut()
+                .unwrap()
+                .add_record(Uuid::new_v4().to_string(), v);
+            self.maybe_flush_with_retries(ctx)
                 .await
                 .expect("failed to flush batch during processing");
-        } else {
-            self.in_progress_batch = Some(batch_preparer);
         }
     }
 
     async fn handle_checkpoint(&mut self, _: CheckpointBarrier, _: &mut ArrowContext) {
-        if let Some(batch_preparer) = self.in_progress_batch.take() {
-            batch_preparer
-                .flush()
-                .await
-                .expect("failed to flush batch during checkpoint");
-        }
+        retry!(
+            self.in_progress_batch.as_mut().unwrap().flush().await,
+            30,
+            Duration::from_millis(100),
+            Duration::from_secs(10),
+            |e| warn!("{}", e)
+        )
+        .expect("could not flush to Kinesis during checkpointing");
     }
 
-    async fn handle_tick(&mut self, _: u64, _ctx: &mut ArrowContext) {
-        let Some(batch_preparer) = &self.in_progress_batch else {
-            return;
-        };
-
-        if !self.flush_config.should_flush(batch_preparer) {
-            return;
-        }
-        let in_progress_batch = self.in_progress_batch.take().unwrap();
-
-        self.flush_with_retries(in_progress_batch)
+    async fn handle_tick(&mut self, _: u64, ctx: &mut ArrowContext) {
+        self.maybe_flush_with_retries(ctx)
             .await
             .expect("failed to flush batch during tick");
     }
 }
 
 impl KinesisSinkFunc {
-    async fn flush_with_retries(
-        &mut self,
-        mut record_batch_preparer: BatchRecordPreparer,
-    ) -> Result<()> {
-        let mut retries = 0;
-        loop {
-            let vectors_to_retry = record_batch_preparer.flush().await?;
-            if vectors_to_retry.is_empty() {
-                return Ok(());
-            } else {
-                retries += 1;
-                warn!("failed to flush batch, retry attempt: {}", retries);
-                tokio::time::sleep(std::time::Duration::from_millis(2000.min(100 << retries)))
-                    .await;
-                record_batch_preparer = self.take_or_create_batch_preparer().await;
-                for (k, v) in vectors_to_retry {
-                    record_batch_preparer.add_record(k, v);
-                }
-            }
+    async fn maybe_flush_with_retries(&mut self, ctx: &mut ArrowContext) -> Result<()> {
+        if !self
+            .flush_config
+            .should_flush(self.in_progress_batch.as_ref().unwrap())
+        {
+            return Ok(());
         }
-    }
 
-    async fn take_or_create_batch_preparer(&mut self) -> BatchRecordPreparer {
-        match self.in_progress_batch.take() {
-            None => BatchRecordPreparer::new(
-                self.client
-                    .as_ref()
-                    .unwrap()
-                    .put_records()
-                    .stream_name(self.name.clone()),
-            ),
-            Some(batch_preparer) => batch_preparer,
-        }
+        retry!(
+            self.in_progress_batch.as_mut().unwrap().flush().await,
+            20,
+            Duration::from_millis(100),
+            Duration::from_secs(2),
+            |e| ctx
+                .report_error("failed to write to Kinesis", format!("{:?}", e))
+                .await
+        )?;
+
+        Ok(())
     }
 }
 
 pub struct BatchRecordPreparer {
-    // TODO: figure out how to not need an option
-    put_records_call: Option<PutRecords>,
+    client: Arc<KinesisClient>,
+    stream: String,
     buffered_records: Vec<(String, Vec<u8>)>,
-    record_count: usize,
     data_size: usize,
-    creation_time: SystemTime,
+    last_flush_time: Instant,
 }
 
 pub struct FlushConfig {
@@ -148,70 +119,98 @@ impl FlushConfig {
     ) -> Self {
         Self {
             max_record_count: records_per_batch.unwrap_or(500) as usize,
-            max_data_size: max_buffer_size.unwrap_or(4_500_000) as usize,
+            max_data_size: max_buffer_size.unwrap_or(4_000_000) as usize,
             max_age: Duration::from_millis(flush_interval_millis.unwrap_or(1000) as u64),
         }
     }
 
     fn should_flush(&self, batch_preparer: &BatchRecordPreparer) -> bool {
-        batch_preparer.record_count >= self.max_record_count
+        batch_preparer.buffered_records.len() >= self.max_record_count
             || batch_preparer.data_size >= self.max_data_size
-            || batch_preparer.creation_time.elapsed().unwrap_or_default() >= self.max_age
+            || batch_preparer.last_flush_time.elapsed() >= self.max_age
     }
 }
 
 impl BatchRecordPreparer {
-    fn new(put_records_call: PutRecords) -> Self {
+    fn new(client: Arc<KinesisClient>, name: String) -> Self {
         Self {
-            put_records_call: Some(put_records_call),
+            client,
+            stream: name,
             buffered_records: Vec::new(),
-            record_count: 0,
             data_size: 0,
-            creation_time: SystemTime::now(),
+            last_flush_time: Instant::now(),
         }
     }
     fn add_record(&mut self, key: String, value: Vec<u8>) {
-        self.buffered_records.push((key.clone(), value.clone()));
-        let blob = Blob::new(value);
-        self.data_size += blob.as_ref().len();
-        let put_record_request = PutRecordsRequestEntry::builder()
-            .data(blob)
-            .partition_key(key)
-            .build();
-        self.put_records_call = self
-            .put_records_call
-            .take()
-            .map(|call| call.records(put_record_request));
-        self.record_count += 1;
+        self.data_size += value.len() + key.len();
+        self.buffered_records.push((key, value));
     }
 
-    async fn flush(mut self) -> Result<Vec<(String, Vec<u8>)>> {
-        if self.record_count == 0 {
-            return Ok(Vec::new());
+    async fn flush(&mut self) -> Result<()> {
+        if self.buffered_records.is_empty() {
+            return Ok(());
         }
-        let response = self.put_records_call.take().unwrap().send().await?;
+
+        let response = self
+            .client
+            .put_records()
+            .stream_name(self.stream.clone())
+            .set_records(Some(
+                self.buffered_records
+                    .iter()
+                    .map(|(k, v)| {
+                        PutRecordsRequestEntry::builder()
+                            .partition_key(k)
+                            .data(Blob::new(v.clone()))
+                            .build()
+                            // cannot occur when partition key and data are supplied
+                            .unwrap()
+                    })
+                    .collect(),
+            ))
+            .send()
+            .await?;
+
+        self.last_flush_time = Instant::now();
+
         let failed_record_count = response.failed_record_count().unwrap_or(0);
-        if failed_record_count > 0 {
-            warn!(
-                "batch write had {} failed responses out of {}",
-                failed_record_count, self.record_count
-            );
-            let records_to_retry = response
-                .records()
-                .unwrap()
-                .iter()
-                .enumerate()
-                .filter_map(|(i, record)| {
-                    if record.error_code().is_some() {
-                        Some(self.buffered_records[i].clone())
-                    } else {
-                        None
-                    }
-                })
-                .collect();
-            Ok(records_to_retry)
-        } else {
-            Ok(Vec::new())
+        if failed_record_count == 0 {
+            self.buffered_records.clear();
+            self.data_size = 0;
+            return Ok(());
         }
+
+        let records_to_retry: HashSet<_> = response
+            .records()
+            .iter()
+            .enumerate()
+            .filter_map(|(i, record)| {
+                if record.error_code().is_some() {
+                    Some(i)
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        self.buffered_records = self
+            .buffered_records
+            .drain(..)
+            .enumerate()
+            .filter(|(i, _)| records_to_retry.contains(i))
+            .map(|(_, v)| v)
+            .collect();
+
+        self.data_size = self
+            .buffered_records
+            .iter()
+            .map(|(k, v)| k.len() + v.len())
+            .sum();
+
+        bail!(
+            "batch write had {} failed responses out of {}",
+            failed_record_count,
+            self.buffered_records.len()
+        )
     }
 }
