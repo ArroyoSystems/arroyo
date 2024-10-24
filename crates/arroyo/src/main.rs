d5068df88258f26765731add8f8bc501e5ee4cb9
zhuliquan <zlqlovecode@foxmail.com>
2024-10-24 14:55:56 +0800

    feat: make heap profiling as default

--- crates/arroyo/src/main.rs
+++ crates/arroyo/src/main.rs
@@ -33,7 +33,6 @@ use uuid::Uuid;
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
 #[cfg(all(
-    feature = "profiling",
     not(target_env = "msvc"),
     not(target_os = "macos"),
     any(target_arch = "x86_64", target_arch = "aarch64")
