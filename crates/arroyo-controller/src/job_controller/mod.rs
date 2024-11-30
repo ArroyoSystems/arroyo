33472d4874ea5ffe24a59700afe5ab644d85cf87
Micah Wylde <micah@arroyo.systems>
2024-11-30 15:14:36 -0800

    chaining optimizer

--- crates/arroyo-controller/src/job_controller/mod.rs
+++ crates/arroyo-controller/src/job_controller/mod.rs
@@ -656,6 +656,11 @@ impl JobController {
             .map(|(id, w)| (*id, w.connect.clone()))
             .collect();
         let program = self.model.program.clone();
+        let operator_indices: Arc<HashMap<_, _>> = Arc::new(program.graph
+            .node_indices()
+            .map(|idx| (program.graph[idx].node_id, idx.index() as u32))
+            .collect());
+        
 
         self.model.metric_update_task = Some(tokio::spawn(async move {
             let mut metrics: HashMap<(u32, u32), HashMap<MetricName, u64>> = HashMap::new();
@@ -682,12 +687,12 @@ impl JobController {
                     .into_iter()
                     .filter_map(|f| Some((get_metric_name(&f.name?)?, f.metric)))
                     .flat_map(|(metric, values)| {
-                        let program = program.clone();
+                        let operator_indices = operator_indices.clone();
                         values.into_iter().filter_map(move |m| {
                             let subtask_idx =
                                 u32::from_str(find_label(&m.label, "subtask_idx")?).ok()?;
                             let operator_idx =
-                                program.operator_index(u32::from_str(find_label(&m.label, "node_id")?).ok()?)?;
+                                *operator_indices.get(&u32::from_str(find_label(&m.label, "node_id")?).ok()?)?;
                             let value = m
                                 .counter
                                 .map(|c| c.value)
