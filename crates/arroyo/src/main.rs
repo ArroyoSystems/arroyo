b670abe32310f58cc4c41c77331247ae51d2d477
Micah Wylde <mwylde@cloudflare.com>
2026-01-07 14:12:23 -0800

    Update compiler to 1.92

--- crates/arroyo/src/main.rs
+++ crates/arroyo/src/main.rs
@@ -38,7 +38,7 @@ static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
     any(target_arch = "x86_64", target_arch = "aarch64")
 ))]
 #[allow(non_upper_case_globals)]
-#[export_name = "malloc_conf"]
+#[unsafe(export_name = "malloc_conf")]
 pub static malloc_conf: &[u8] = b"prof:true,prof_active:true,lg_prof_sample:19\0";
 
 #[derive(Parser)]
