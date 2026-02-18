ArroyoSystems/arroyo

commit f940d30af308cb02e1b1be2d2939a46406ca694b

Fix various nitpicking/typos (#1009)

crates/arroyo-api/src/connection_tables.rs
@@ -694,7 +694,7 @@ async fn expand_json_schema(
                         schema_response.0.schema_type
                     )));
                 }
-                confluent_schema_id.replace(schema_response.0.version);
+                confluent_schema_id.replace(schema_response.0.id);
 
                 schema.definition = Some(SchemaDefinition::JsonSchema {
                     schema: schema_response.0.schema,

crates/arroyo-connectors/src/filesystem/sink/iceberg/mod.rs
@@ -161,8 +161,8 @@ impl IcebergTable {
         task_info: Arc<TaskInfo>,
         schema: &Schema,
     ) -> Result<&Table, DataflowError> {
-        if self.table.is_some() {
-            return Ok(self.table.as_ref().unwrap());
+        if let Some(ref table) = self.table {
+            return Ok(table);
         }
 
         self.task_info = Some(task_info);

crates/arroyo-controller/src/lib.rs
@@ -25,8 +25,6 @@ use arroyo_server_common::shutdown::ShutdownGuard;
 use arroyo_server_common::wrap_start;
 use arroyo_types::{MachineId, WorkerId, from_micros};
 use cornucopia_async::DatabaseSource;
-use lazy_static::lazy_static;
-use prometheus::{Gauge, register_gauge};
 use states::{Created, State, StateMachine};
 use std::collections::{HashMap, HashSet};
 use std::env;
@@ -59,24 +57,6 @@ use types::public::LogLevel;
 use types::public::{RestartMode, StopMode};
 
 pub const CHECKPOINTS_TO_KEEP: u32 = 5;
-
-lazy_static! {
-    static ref ACTIVE_PIPELINES: Gauge = register_gauge!(
-        "arroyo_controller_active_pipelines",
-        "number of active pipelines in arroyo-controller"
-    )
-    .unwrap();
-    pub static ref COMPACTION_TUPLES_IN: Gauge = register_gauge!(
-        "arroyo_controller_compaction_tuples_in",
-        "Number of tuples being considered for compaction"
-    )
-    .unwrap();
-    static ref COMPACTION_TUPLES_OUT: Gauge = register_gauge!(
-        "arroyo_controller_compaction_tuples_in",
-        "Number of tuples being considered for compaction"
-    )
-    .unwrap();
-}
 
 #[derive(Eq, PartialEq, Clone, Debug)]
 pub struct JobConfig {

crates/arroyo-planner/src/lib.rs
@@ -499,7 +499,7 @@ impl FunctionRegistry for ArroyoSchemaProvider {
         if let Some(f) = self.window_functions.get(name) {
             Ok(Arc::clone(f))
         } else {
-            plan_err!("No UDAF with name {name}")
+            plan_err!("No UDWF with name {name}")
         }
     }
 

crates/arroyo-planner/src/tables.rs
@@ -408,7 +408,7 @@ impl ConnectorTable {
         table.idle_time = options
             .pull_opt_i64("idle_micros")?
             .or_else(|| DEFAULT_IDLE_TIME.map(|t| t.as_micros() as i64))
-            .filter(|t| *t <= 0)
+            .filter(|t| *t > 0)
             .map(|t| Duration::from_micros(t as u64));
 
         table.lookup_cache_max_bytes = options.pull_opt_u64("lookup.cache.max_bytes")?;

crates/arroyo-worker/src/engine.rs
@@ -306,7 +306,7 @@ impl Program {
                 .node_indices()
                 .filter(|n| physical.node_weight(*n).unwrap().id() == logical_out_node.node_id)
                 .collect();
-            assert_ne!(from_nodes.len(), 0, "failed to find to nodes");
+            assert_ne!(to_nodes.len(), 0, "failed to find to nodes");
 
             match edge.edge_type {
                 LogicalEdgeType::Forward => {

commit 9366f07cef2d7a406ae0b172b16e73ee140be332

Partial revert of f940d30af308cb02e1b1be2d2939a46406ca694b
A regression was introduced in commit: f940d30af308cb02e1b1be2d2939a46406ca694b
PR: #1009

crates/arroyo-api/src/connection_tables.rs
@@ -694,7 +694,7 @@ async fn expand_json_schema(
                         schema_response.0.schema_type
                     )));
                 }
-                confluent_schema_id.replace(schema_response.0.id);
+                confluent_schema_id.replace(schema_response.0.version);
 
                 schema.definition = Some(SchemaDefinition::JsonSchema {
                     schema: schema_response.0.schema,